8f8c688255e4eb06747522e726dba94520283abd
Alexandre <xande.haus@gmail.com>
2024-07-14 20:01:49 -0300

    Verificação final, adicionado no TXT oque  falta fazer

--- final_project_lince_tech_academy/macos/Flutter/GeneratedPluginRegistrant.swift
+++ final_project_lince_tech_academy/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import file_selector_macos
 import path_provider_foundation
+import printing
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
@@ -14,6 +15,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
